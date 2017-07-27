69089a22f55db74e5d50ad64eaddfee185ddbd93
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-07-27 23:32:28 +0200

    Added checks

--- src/main/java/org/aurora/tag/command/TagCommand.java
+++ src/main/java/org/aurora/tag/command/TagCommand.java
@@ -5,7 +5,23 @@ import org.bukkit.command.CommandSender;
 public class TagCommand {
 	
 	public static void handle(CommandSender sender, String[] args) {
-		
+		// Check if the user has the necessary permissions
+		if(args[0].equalsIgnoreCase("join")) {
+			if(sender.hasPermission("tag.join"))
+				
+			else
+				notEnoughPermission(sender);
+		}
+		else if(args[1].equalsIgnoreCase("start")) {
+			if(sender.hasPermission("tag.start"))
+				
+			else
+				notEnoughPermission(sender);
+		}
+	}
+	
+	private static void notEnoughPermission(CommandSender sender) {
+		sender.sendMessage("You do not have the proper permissions to execute this command!");
 	}
 
 }
