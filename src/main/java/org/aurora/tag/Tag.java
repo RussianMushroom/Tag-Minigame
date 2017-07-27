eacc98c2135abb251e6a14760cb075e379c837ec
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-07-27 22:38:31 +0200

    Added defaults

--- src/main/java/org/aurora/tag/Tag.java
+++ src/main/java/org/aurora/tag/Tag.java
@@ -1,10 +1,22 @@
 package org.aurora.tag;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * @author RussianMushroom
  *
  */
-public class Tag {
+public class Tag extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
 
 }
 
