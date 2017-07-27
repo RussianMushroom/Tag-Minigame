9aebde0d67bd2cab3f09006bedb4f354cfdbcc5c
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-07-27 22:56:47 +0200

    Implemented a command handler

--- src/main/java/org/aurora/tag/Tag.java
+++ src/main/java/org/aurora/tag/Tag.java
@@ -1,5 +1,8 @@
 package org.aurora.tag;
 
+import org.aurora.tag.command.TagCommand;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -18,5 +21,12 @@ public class Tag extends JavaPlugin {
 		super.onDisable();
 	}
 
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(label.equalsIgnoreCase("tag"))
+			TagCommand.handle(sender, args);
+		return super.onCommand(sender, command, label, args);
+	}
+	
 }
 
---
+++ src/main/java/org/aurora/tag/command/TagCommand.java
@@ -0,0 +1,11 @@
+package org.aurora.tag.command;
+
+import org.bukkit.command.CommandSender;
+
+public class TagCommand {
+	
+	public static void handle(CommandSender sender, String[] args) {
+		
+	}
+
+}
