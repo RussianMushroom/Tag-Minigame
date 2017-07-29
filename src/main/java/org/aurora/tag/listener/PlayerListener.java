361d7d9d942a1b78b10e4edf4153c5467b3a5ccf
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-07-29 23:40:57 +0200

    Renamed

--- src/main/java/org/aurora/tag/listener/PlayerListener.java
+++ src/main/java/org/aurora/tag/listener/PlayerListener.java
@@ -5,7 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
-public class TagListener implements Listener 
+public class PlayerListener implements Listener 
 {
 	public void onPlayerInteractEntityEvent(PlayerInteractEntityEvent event)
 	{
