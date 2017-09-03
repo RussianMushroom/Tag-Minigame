10f571f0c3e6f54339e7de9942c134d453945fd0
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-09-03 11:47:45 +0200

    Added a 10% chance to get a special armour.

--- src/main/java/org/aurora/tag/util/InventoryManager.java
+++ src/main/java/org/aurora/tag/util/InventoryManager.java
@@ -1,6 +1,7 @@
 package org.aurora.tag.util;
 
 import java.util.Arrays;
+import java.util.Random;
 
 import org.aurora.tag.config.ConfigLoader;
 import org.aurora.tag.game.GameCenter;
@@ -13,6 +14,8 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.potion.PotionData;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.potion.PotionType;
 
 import io.netty.util.internal.ThreadLocalRandom;
@@ -26,12 +29,18 @@ import io.netty.util.internal.ThreadLocalRandom;
 public class InventoryManager {
 	
 	public static void clearPlayerInventory(boolean stopping, String arena) {
+		
 		GameCenter.getArena(arena).getVotedPlayers().forEach(player -> {
+			player.getInventory().clear();
+		});
+			/*
 			if(stopping) {
 				// clear the game inventory and load player's saved inventory
 				player.getInventory().clear();
-				player.getInventory().setContents(
-						GameCenter.getArena(arena).getPlayerInv().get(player));
+				
+				ItemStack[] playerInv = GameCenter.getArena(arena).getPlayerInv().get(player);
+				if(playerInv != null)
+					player.getInventory().setContents(playerInv);
 			} else {
 				// Save player's inventory
 				GameCenter.getArena(arena).addPlayerInv(
@@ -39,12 +48,16 @@ public class InventoryManager {
 				player.getInventory().clear();
 			}
 		});
+		*/
 	}
 	
 	public static void restoreInv(Player player, TagArena arena) {
 		player.getInventory().clear();
+		/*
+		player.getInventory().clear();
 		player.getInventory().setContents(
 				arena.getPlayerInv().get(player));
+				*/
 	}
 	
 	public static void setTagBaton(String arena) {
@@ -62,68 +75,98 @@ public class InventoryManager {
 	
 	public static void setArmour(String arena) {
 		Color armourColour = Color.BLUE;
+		Color customArmourColour = Color.ORANGE;
 		ItemStack[] iStack = new ItemStack[] {
 				new ItemStack(Material.LEATHER_BOOTS),
 				new ItemStack(Material.LEATHER_LEGGINGS),
 				new ItemStack(Material.LEATHER_CHESTPLATE),
 				new ItemStack(Material.LEATHER_HELMET)
 		};
+		setItemMeta(iStack, armourColour);
 		
-		Arrays.asList(iStack).forEach(stack -> {
-			LeatherArmorMeta lMeta = (LeatherArmorMeta) stack.getItemMeta();
-			
-			lMeta.setColor(armourColour);
-			stack.setItemMeta(lMeta);
-		});
+		ItemStack[] customItemStack = new ItemStack[] {
+				new ItemStack(Material.LEATHER_BOOTS),
+				new ItemStack(Material.LEATHER_LEGGINGS),
+				new ItemStack(Material.LEATHER_CHESTPLATE),
+				new ItemStack(Material.PUMPKIN)
+		};
+		setItemMeta(customItemStack, customArmourColour);
 		
 		GameCenter.getArena(arena).getVotedPlayers().forEach(player -> {
-			player.getInventory().setArmorContents(iStack);
+			Random rand = new Random();
+			if(rand.nextDouble() < Double.parseDouble(
+							ConfigLoader.getDefault("Tag.Tools.ChanceToGetPumpkin")))
+				player.getInventory().setArmorContents(customItemStack);
+			else
+				player.getInventory().setArmorContents(iStack);
+		});
+	}
+	
+	private static void setItemMeta(ItemStack[] items, Color colour) {
+		// in this specific case
+		Arrays.asList(items).forEach(stack -> {
+			if(stack.getType() != Material.PUMPKIN) {
+				LeatherArmorMeta lMeta = (LeatherArmorMeta) stack.getItemMeta();
+				
+				lMeta.setColor(colour);
+				stack.setItemMeta(lMeta);
+			}
 		});
 	}
 
 	public static void setUpgrade(Player player) {
+		Random rand = new Random();
+		Double configChance = Double.parseDouble(
+				ConfigLoader.getDefault("Tag.Tools.ChanceToGetEasterEgg"));
 		ItemStack bow = new ItemStack(Material.BOW),
 				arrow = new ItemStack(Material.ARROW, 
 						Integer.parseInt(ConfigLoader.getDefault("Tag.Tools.ArrowCount"))),
-				healPotion = new ItemStack(Material.LINGERING_POTION),
+				// healPotion = new ItemStack(Material.LINGERING_POTION),
 				swiftPotion = new ItemStack(Material.LINGERING_POTION),
-				jumpPotion = new ItemStack(Material.LINGERING_POTION);
-				// blindPotion = new ItemStack(Material.LINGERING_POTION);
+				jumpPotion = new ItemStack(Material.LINGERING_POTION),
+				blindPotion = new ItemStack(Material.LINGERING_POTION);
 		
 		// Add meta data to the potions
-		PotionMeta healMeta = (PotionMeta) healPotion.getItemMeta();
-		healMeta.setBasePotionData(new PotionData(PotionType.INSTANT_HEAL,
-				false, false));
-		healPotion.setItemMeta(healMeta);
+		// PotionMeta healMeta = (PotionMeta) healPotion.getItemMeta();
+		// healMeta.setBasePotionData(new PotionData(PotionType.INSTANT_HEAL,
+		//		false, false));
+		// healPotion.setItemMeta(healMeta);
 		
 		PotionMeta speedMeta = (PotionMeta) swiftPotion.getItemMeta();
 		speedMeta.setBasePotionData(new PotionData(PotionType.SPEED,
 				false, false));
+		if(rand.nextDouble() < configChance)
+			speedMeta.setDisplayName(ConfigLoader.getDefault("Tag.Tools.SpeedPotionName"));
 		swiftPotion.setItemMeta(speedMeta);
 		
 		PotionMeta jumpMeta = (PotionMeta) jumpPotion.getItemMeta();
 		jumpMeta.setBasePotionData(new PotionData(PotionType.JUMP,
 				false, false));
+		if(rand.nextDouble() < configChance)
+			jumpMeta.setDisplayName(ConfigLoader.getDefault("Tag.Tools.JumpPotionName"));
 		jumpPotion.setItemMeta(jumpMeta);
 		
-		/*
+		
 		PotionMeta blindMeta = (PotionMeta) blindPotion.getItemMeta();
-		blindMeta.setMainEffect(PotionEffectType.BLINDNESS);
+		blindMeta.addCustomEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 1), true);
+		if(rand.nextDouble() < configChance)
+			blindMeta.setDisplayName(ConfigLoader.getDefault("Tag.Tools.BlindPotionName"));
 		blindPotion.setItemMeta(blindMeta);
-		*/
+		
 		
 		// Add items to an array
-		ItemStack[] items = new ItemStack[] {bow, arrow, healPotion, swiftPotion, jumpPotion /*, blindPotion*/};
+		ItemStack[] items = new ItemStack[] {bow, arrow, blindPotion, swiftPotion, jumpPotion /*, blindPotion*/};
 		ItemStack upgrade = items[randomIndexGenerator(items.length)];
 		
-		if(upgrade.getType() == Material.BOW) {
+		if(upgrade.getType() == Material.BOW)
 			player.getInventory().addItem(arrow);
+		else if(upgrade.getType() == Material.ARROW && !player.getInventory().contains(Material.BOW))
+			player.getInventory().addItem(bow);
 			
-			if(player.getInventory().contains(Material.BOW)) 
-				upgrade = arrow;
-		}
+		if(player.getInventory().contains(Material.BOW)) 
+			upgrade = arrow;
 		
-			player.getInventory().addItem(upgrade);
+		player.getInventory().addItem(upgrade);
 	}
 	
 	/**
