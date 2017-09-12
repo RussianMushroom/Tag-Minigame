7f81ab3cf1450105680c58b8529a20b1f18a6ae0
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-09-12 19:19:19 +0200

    Added classto contain general common methods used by a variety of other classes.

---
+++ src/main/java/org/aurora/tag/util/GeneralMethods.java
@@ -0,0 +1,32 @@
+package org.aurora.tag.util;
+
+import org.aurora.tag.game.TagArena;
+import org.bukkit.ChatColor;
+
+/**
+ * 
+ * @author RussianMushroom
+ *
+ */
+public class GeneralMethods {
+
+	public static void displayMessage(TagArena arena, String message) {
+		arena.getVotedPlayers().forEach(player -> {
+			player.sendMessage(ChatColor.GOLD + message);
+		});
+	}
+	
+	public static String toProperCase(String word) {
+		return word.substring(0, 1).toUpperCase() + word.substring(1, word.length());
+	}
+	
+	public static boolean isInteger(String string) {
+		try {
+			Integer.parseInt(string);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+	
+}
