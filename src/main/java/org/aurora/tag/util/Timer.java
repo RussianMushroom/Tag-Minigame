3e5619097d680532cccc7141e1ee56df9c7f25d8
RussianMushroom <RussianMushroom@users.noreply.github.com>
2017-09-15 17:56:12 +0200

    Timer now warns 3 seconds before the game starts.

--- src/main/java/org/aurora/tag/util/Timer.java
+++ src/main/java/org/aurora/tag/util/Timer.java
@@ -2,6 +2,7 @@ package org.aurora.tag.util;
 
 import org.aurora.tag.config.ConfigLoader;
 import org.aurora.tag.game.GameCenter;
+import org.aurora.tag.game.TagArena;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
@@ -12,12 +13,15 @@ import org.bukkit.scheduler.BukkitTask;
  * @author RussianMushroom
  */
 public class Timer {
+
+	 private Integer taskId;
+	 private int count = 3;
 	
 	 public void startUpgradeTimer(BukkitTask upgradeTask, String arena) {
 		 GameCenter.getArena(arena).setUpgradeIsActive(false);
 		 Runnable run = () -> {
 			 GameCenter.getArena(arena).getVotedPlayers().forEach(player -> {
-				 player.getPlayer().sendMessage(ChatColor.GOLD
+				 player.sendMessage(ChatColor.GOLD
 						 + ConfigLoader.getDefault("Tag.Strings.GetUpgrade"));
 			 });
 			 GameCenter.getArena(arena).setUpgradeIsActive(true);
@@ -30,38 +34,75 @@ public class Timer {
 				 );
 	 }
 	 
-	 public void startGraceTimer(BukkitTask graceTask, String arena) {
-		 GameCenter.getArena(arena).setGrace(true);
+	 public void startGraceTimer(BukkitTask graceTask, String tagArena) {
+		 TagArena arena = GameCenter.getArena(tagArena);
+		 boolean displayHologram = ConfigLoader.getFileConfig().getBoolean("Tag.Options.AllowHolographicCountdown");
 		 
-		 GameCenter.getArena(arena).getVotedPlayers().forEach(player -> {
-			 player.getPlayer().sendMessage(ChatColor.GOLD
+		 arena.setGrace(true);
+		 
+		 arena.getVotedPlayers().forEach(player -> {
+			 player.sendMessage(ChatColor.GOLD
 					 + String.format(ConfigLoader.getDefault("Tag.Strings.Grace"),
 					 Integer.parseInt(ConfigLoader.getDefault("Tag.Timer.GracePeriod")) / 20));
 		 });
 			 
 			 Runnable run = () -> {
-				 GameCenter.getArena(arena).getVotedPlayers().forEach(player -> {
-					 player.getPlayer().sendMessage(ChatColor.GOLD
-							 + ConfigLoader.getDefault("Tag.Strings.NoGrace"));
-					 player.getPlayer().playSound(
-							 player.getPlayer().getLocation(),
+				 arena.getVotedPlayers().forEach(player -> {
+					 if(displayHologram && Bukkit.getServer().getPluginManager().isPluginEnabled("ProtocolLib"))
+						 HologramManager.display(ConfigLoader.getDefault("Tag.Strings.NoGrace"),
+								 ConfigLoader.getDefault("Tag.Strings.GracePeriodGoodLuck"), player);
+					 else
+						 player.sendMessage(ChatColor.GOLD + ConfigLoader.getDefault("Tag.Strings.NoGrace") 
+						 	+ " " + ConfigLoader.getDefault("Tag.Strings.GracePeriodGoodLuck"));
+					 player.playSound(
+							 player.getLocation(),
 							 Sound.ENTITY_LIGHTNING_THUNDER, 10, 1);
 				 });
 
-				 GameCenter.getArena(arena).setGrace(false);
+				 arena.setGrace(false);
 			 };
 				 
 			 graceTask = Bukkit.getServer().getScheduler().runTaskLater(
 					 Bukkit.getServer().getPluginManager().getPlugin("Tag"),
 					 run,
 					 Long.parseLong(ConfigLoader.getDefault("Tag.Timer.GracePeriod")));
+			 
+			 // start a timer that starts 3 seconds earlier than the one above
+			 long countdown = Long.parseLong(ConfigLoader.getDefault("Tag.Timer.GracePeriod")) - 60; 
+			 
+			 taskId = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(
+					 Bukkit.getServer().getPluginManager().getPlugin("Tag"),
+					 () -> {
+						 displayTimeLeft(arena, displayHologram);
+					 }, countdown, 20);
+	 }
+	 
+	 private void displayTimeLeft(TagArena arena, boolean displayHologram) {
+		 if(count == 1)
+			 cancelCountdownTask(); 
+		 
+		 // TODO add check for ProtocolLib
+		 arena.getVotedPlayers().forEach(player -> {
+			 if(displayHologram && Bukkit.getServer().getPluginManager().isPluginEnabled("ProtocolLib"))
+				 HologramManager.display(ConfigLoader.getDefault("Tag.Strings.GracePeriodEndsIn"),
+						 Integer.toString(count), player);
+			 else
+				 player.sendMessage(ChatColor.GOLD + ConfigLoader.getDefault("Tag.Strings.GracePeriodEndsIn")
+				 	+ ": " + ChatColor.YELLOW + count);
+		 });
+		 
+		 count--;
+	 }
+	 
+	 private void cancelCountdownTask() {
+		 Bukkit.getScheduler().cancelTask(taskId);
 	 }
 	 
 	 public void delayStart(String arena) {
 		// Set game to active
 		GameCenter.getArena(arena).activate();
 		GameCenter.getArena(arena).getVotedPlayers().forEach(player -> {
-			 player.getPlayer().sendMessage(ChatColor.GOLD
+			 player.sendMessage(ChatColor.GOLD
 					 + String.format(ConfigLoader.getDefault("Tag.Strings.NotifyPlayersGameStart"),
 							 Integer.parseInt(ConfigLoader.getDefault("Tag.Timer.TicksBeforeTagStart")) / 20));
 		 });
